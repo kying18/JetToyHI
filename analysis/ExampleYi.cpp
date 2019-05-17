d7b714ab233505d548b8ef4ab5d8f080409cefa8
FHead <chen.yi.first@gmail.com>
2019-05-17 21:51:00 +0200

    updated example file

--- analysis/ExampleYi.cpp
+++ analysis/ExampleYi.cpp
@@ -1,3 +1,4 @@
+#include <vector>
 #include <iostream>
 using namespace std;
 
@@ -18,15 +19,15 @@ int main(int argc, char *argv[])
 
    CommandLine CL(argc, argv);
 
-   string InputFileName = CL.Get("input");
-   string OutputFileName = CL.Get("output", "Plots.pdf");
+   string OutputBase = CL.Get("output", "Plots");
+   vector<string> InputFileName = CL.GetStringVector("input");
+   bool DoJewel = CL.GetBool("jewel", false);
+   string Radius = CL.Get("R", "04");
 
-   TFile File(InputFileName.c_str());
-
-   Messenger M(File, "JetTree");
-
-   PdfFileHelper PdfFile(OutputFileName);
+   PdfFileHelper PdfFile(OutputBase + ".pdf");
    PdfFile.AddTextPage("Plots");
+
+   TFile OutputFile((OutputBase + ".root").c_str(), "RECREATE");
 
    TH1D HPhotonPT("HPhotonPT", "Photon PT;PT;", 100, 0, 1000);
    TH1D HPhotonEta("HPhotonEta", "Photon Eta;Eta;", 100, -3.0, 3.0);
@@ -34,54 +35,89 @@ int main(int argc, char *argv[])
    TH1D HJetPT("HJetPT", "Jet PT;PT;", 100, 0, 1000);
    TH1D HJetEta("HJetEta", "Jet Eta;Eta;", 100, -3.0, 3.0);
    TH1D HDPhiAJ("HDPhiAJ", "DPhi(photon, jet);;", 100, -3.14, 3.14);
-   TH1D HDPhi("HDPhi", "DPhi(E-scheme, WTA-scheme);;", 100, -0.3, 0.3);
-   TH1D HDEta("HDEta", "DEta(E-scheme, WTA-scheme);;", 100, -0.3, 0.3);
-   TH1D HDR("HDR", "DR(E-scheme, WTA-scheme);;", 100, 0.0, 0.3);
-
-   for(int iE = 0; iE < M.GetEntries(); iE++)
-   {
-      M.GetEntry(iE);
+   TH1D HDPhi("HDPhi", "DPhi(E-scheme, WTA-scheme);;", 100, -0.5, 0.5);
+   TH1D HDEta("HDEta", "DEta(E-scheme, WTA-scheme);;", 100, -0.5, 0.5);
+   TH1D HDR("HDR", "DR(E-scheme, WTA-scheme);;", 100, 0.0, 0.5);
    
-      if(M.LeadingPhoton.Pt() < 10)
-         continue;
+   for(string FileName : InputFileName)
+   {
+      TFile File(FileName.c_str());
 
-      HPhotonPT.Fill(M.LeadingPhoton.Pt());
-      HPhotonEta.Fill(M.LeadingPhoton.Eta());
+      Messenger M(File, "JetTree");
 
-      HNJet.Fill(M.SignalJetR04.size());
-
-      int JetIndex = -1;
-
-      for(int iJ = 0; iJ < M.SignalJetR04.size(); iJ++)
+      for(int iE = 0; iE < M.GetEntries(); iE++)
       {
-         HJetPT.Fill(M.SignalJetR04[iJ].P.Pt());
-         HJetEta.Fill(M.SignalJetR04[iJ].P.Eta());
-         
-         double DPhiAJ = M.SignalJetR04[iJ].P.Phi() - M.LeadingPhoton.Phi();
-         if(DPhiAJ > +M_PI)   DPhiAJ = DPhiAJ - 2 * M_PI;
-         if(DPhiAJ < -M_PI)   DPhiAJ = DPhiAJ + 2 * M_PI;
+         M.GetEntry(iE);
 
-         if(DPhiAJ > -M_PI / 2 && DPhiAJ < M_PI / 2)
+         if(M.LeadingPhoton.Pt() < 10)
+            continue;
+         if(M.LeadingPhoton.Eta() < -2 || M.LeadingPhoton.Eta() > 2)
             continue;
 
-         JetIndex = iJ;
-         break;
+         HPhotonPT.Fill(M.LeadingPhoton.Pt());
+         HPhotonEta.Fill(M.LeadingPhoton.Eta());
+
+         vector<Jet> Jets;
+         if(Radius == "02")   Jets = M.SignalJetR02;
+         if(Radius == "04")   Jets = M.SignalJetR04;
+         if(Radius == "06")   Jets = M.SignalJetR06;
+         if(Radius == "08")   Jets = M.SignalJetR08;
+         if(Radius == "10")   Jets = M.SignalJetR10;
+
+         HNJet.Fill(Jets.size());
+
+         int JetIndex = -1;
+
+         for(int iJ = 0; iJ < Jets.size(); iJ++)
+         {
+            TLorentzVector P;
+            if(DoJewel == true)
+               P = Jets[iJ].JewelP;
+            else
+               P = Jets[iJ].P;
+
+            if(P.Pt() < 10)
+               continue;
+            if(P.Eta() < -2.5 || P.Eta() > 2.0)
+               continue;
+
+            HJetPT.Fill(P.Pt());
+            HJetEta.Fill(P.Eta());
+
+            double DPhiAJ = P.Phi() - M.LeadingPhoton.Phi();
+            if(DPhiAJ > +M_PI)   DPhiAJ = DPhiAJ - 2 * M_PI;
+            if(DPhiAJ < -M_PI)   DPhiAJ = DPhiAJ + 2 * M_PI;
+
+            if(DPhiAJ > -M_PI / 2 && DPhiAJ < M_PI / 2)
+               continue;
+
+            JetIndex = iJ;
+            break;
+         }
+
+         if(JetIndex >= 0)
+         {
+            TLorentzVector P;
+            if(DoJewel == true)
+               P = Jets[JetIndex].JewelP;
+            else
+               P = Jets[JetIndex].P;
+
+            double DPhiAJ = P.Phi() - M.LeadingPhoton.Phi();
+            double DPhi = P.Phi() - Jets[JetIndex].WTA.Phi();
+            if(DPhi > +M_PI)   DPhi = DPhi - 2 * M_PI;
+            if(DPhi < -M_PI)   DPhi = DPhi + 2 * M_PI;
+            double DEta = P.Eta() - Jets[JetIndex].WTA.Eta();
+            double DR = sqrt(DEta * DEta + DPhi * DPhi);
+
+            HDPhi.Fill(DPhi);
+            HDEta.Fill(DEta);
+            HDR.Fill(DR);
+            HDPhiAJ.Fill(DPhiAJ);
+         }
       }
 
-      if(JetIndex >= 0)
-      {
-         double DPhiAJ = M.SignalJetR04[JetIndex].P.Phi() - M.LeadingPhoton.Phi();
-         double DPhi = M.SignalJetR04[JetIndex].P.Phi() - M.SignalJetR04[JetIndex].WTA.Phi();
-         if(DPhi > +M_PI)   DPhi = DPhi - 2 * M_PI;
-         if(DPhi < -M_PI)   DPhi = DPhi + 2 * M_PI;
-         double DEta = M.SignalJetR04[JetIndex].P.Eta() - M.SignalJetR04[JetIndex].WTA.Eta();
-         double DR = sqrt(DEta * DEta + DPhi * DPhi);
-
-         HDPhi.Fill(DPhi);
-         HDEta.Fill(DEta);
-         HDR.Fill(DR);
-         HDPhiAJ.Fill(DPhiAJ);
-      }
+      File.Close();
    }
 
    PdfFile.AddPlot(HPhotonPT, "", true);
@@ -96,6 +132,20 @@ int main(int argc, char *argv[])
 
    PdfFile.AddTimeStampPage();
    PdfFile.Close();
+
+   OutputFile.cd();
+
+   HPhotonPT.Write();
+   HPhotonEta.Write();
+   HNJet.Write();
+   HJetPT.Write();
+   HJetEta.Write();
+   HDPhiAJ.Write();
+   HDPhi.Write();
+   HDEta.Write();
+   HDR.Write();
+
+   OutputFile.Close();
 
    return 0;
 }
