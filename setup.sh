9c1495302a41d632e5522d7f8f3debe9deedcd00
Kylie <kying@mit.edu>
2020-10-15 19:32:53 -0400

    Setup

--- setup.sh
+++ setup.sh
@@ -3,5 +3,7 @@
 # export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/afs/cern.ch/sw/lcg/external/MCGenerators_lcgcmt67c/pythia8/226/x86_64-slc6-gcc48-opt/lib
 
 # source /cvmfs/sft.cern.ch/lcg/external/gcc/4.8.1/x86_64-slc6-gcc48-opt/setup.sh
-source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.06.08/x86_64-slc6-gcc48-opt/root/bin/thisroot.sh
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/programs/pythia/pythia8240/lib/:/data/programs/fastjet/install/lib/
+cd /raid5/root/root-v6.16.00-gcc731/bin
+. thisroot.sh
+cd - 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/yjlee/EMMI/pythia/pythia8240/lib/:/data/yjlee/EMMI/fastjet-install/lib/
