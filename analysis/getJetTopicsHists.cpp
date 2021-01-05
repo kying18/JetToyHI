6e2d85c356b811c402da54442f79a0430e8bd2ce
Kylie <kying@mit.edu>
2021-01-05 11:41:40 -0500

    New histogram yeet :)

--- analysis/getJetTopicsHists.cpp
+++ analysis/getJetTopicsHists.cpp
@@ -21,13 +21,21 @@ int main(int argc, char *argv[])
 
    PdfFileHelper PdfFile(OutputBase + ".pdf");
    PdfFile.AddTextPage("Jet Topics Data Plots (pT > " + to_string((int) MinPT) + " GeV)");
+   vector<string> lines;
+   lines.push_back("Pyquen data source location:");
+   lines.push_back("/data/yjlee/pyquen/pp150/pp_1.pu14");
+   lines.push_back("/data/yjlee/pyquen/PbPb150_0_10/PbPb_0_10_1.pu14");
+   lines.push_back("/data/yjlee/pyquen/PbPbWide150_0_10/PbPbWide_0_10_1.pu14");
+   PdfFile.AddTextPage(lines, 0.16, 0.6, 0.03);
+
 
     // stuff for the output csv
-    // ofstream outFile("./example.csv");
-    // ostream_iterator<std::string> output_iterator(output_file, "\n");
+    ofstream outFile("./150_1_histograms.csv", ios_base::app);
+    ostream_iterator<std::string> output_iterator(outFile, "\n");
 
-    // THStack* hNStack = new THStack("hNStack","Constituent Multiplicity;Constituent Multiplicity;1/N dN/d Constituent Multiplicity");
-    // THStack* hPTStack = new THStack("hs","pT;pT;1/N dN/dpT");
+    THStack* hNStack = new THStack("hNStack","Constituent Multiplicity (pyquen dijet samples);Constituent Multiplicity;1/N dN/d Constituent Multiplicity");
+    THStack* hPTStack = new THStack("hPTStack","pT (pyquen dijet samples);pT;1/N dN/dpT");
+    TLegend* legend = new TLegend(0.67,0.75,0.97,0.92);
     // TCanvas* cn = new TCanvas("cn","",1200,900);
     // TCanvas* cpt = new TCanvas("cpt","",1200,900);
 
@@ -41,33 +49,39 @@ int main(int argc, char *argv[])
         // some tools to define the plots
         string titleMod;
         string label;
+        TString legendLabel;
         // string data_label;
 
         int colorIdx;
         if (FileName.find("ZJet") != string::npos){
             titleMod = " (ZJet) (Data: pyquen ppZJet150)";
             label = "pp150_1_zjet";
+            legendLabel = "pp150";
             colorIdx = 1;
             // data_label = "Data using pyquen pp150 samples (pp_1)";
         } else if (FileName.find("PbPbWide") != string::npos){
             titleMod = " (PbPb wide) (Data: pyquen PbPbWide150_0_10)";
             label = "pbpb150_0_10_1_wide";
+            legendLabel = "PbPbWide150";
             colorIdx = 3;
             // data_label = "Data using pyquen pp150 samples (pp_1)";
         } else if (FileName.find("PbPb") != string::npos){
             titleMod = " (PbPb) (Data: pyquen PbPb150_0_10)";
             label = "pbpb150_0_10_1";
+            legendLabel = "PbPb150";
             colorIdx = 4;
             // data_label = "Data using pyquen pp150 samples (pp_1)";
         } else {
             titleMod = " (Dijet) (Data: pyquen pp150)";
             label = "pp150_1";
+            legendLabel = "pp150";
             colorIdx = 2;
             // data_label = "Data using pyquen ppZJet150 samples (pp_1)";
         }
         
         TH1D* HJetN = new TH1D("HJetN", ("Constituent Multiplicity" + titleMod + ";Constitutent Multiplicity;1/N dN").c_str(), 100, 0, 100);
         TH1D* HJetPT = new TH1D("HJetPT", ("Jet PT" + titleMod + ";pT;1/N dN/dpT").c_str(), 100, MinPT, 500);
+        TH2D * h2 = new TH2D("h2", ("pT vs Constituent Multiplicity" + titleMod + ";Constituent Multiplicity;pT").c_str(), 100, 0, 100, 100, MinPT, 500);
         TFile File(FileName.c_str());
 
         TTree *Tree = (TTree *)File.Get("JetTree");
@@ -99,11 +113,11 @@ int main(int argc, char *argv[])
             int NJet = SignalJetPt->size();
             for(int iJ = 0; iJ < NJet; iJ++)
             {
-                if((*SignalJetPt)[iJ] < MinPT)
-                continue;
+                if((*SignalJetPt)[iJ] < MinPT) continue;
                 
                 HJetN->Fill((*SignalJetNConstituent)[iJ], (*Weight)[0]);
                 HJetPT->Fill((*SignalJetPt)[iJ], (*Weight)[0]);
+                h2->Fill((*SignalJetNConstituent)[iJ], (*SignalJetPt)[iJ]);
 
                 nsFiltered.push_back((*SignalJetNConstituent)[iJ]);
                 ptsFiltered.push_back((*SignalJetPt)[iJ]);
@@ -114,7 +128,8 @@ int main(int argc, char *argv[])
 
         TH1D* hists[2] = {HJetN, HJetPT};
         // TH1D* hists[1] = {HJetN};
-        for (auto h : hists) {
+        for (int i=0; i < 2; i++) {
+            TH1D* h = hists[i];
             gStyle->SetTextSize(0.045);
             gStyle->SetTextFont(42);
             gStyle->SetPadRightMargin(0.05);
@@ -122,62 +137,50 @@ int main(int argc, char *argv[])
             gStyle->SetPadTopMargin(0.15);
             gStyle->SetPadBottomMargin(0.145);
             gStyle->SetTitleX(0.5);
-            TCanvas* c = new TCanvas("c","",1200,900);
+            // TCanvas* c = new TCanvas("c","",1200,900);
             h->GetYaxis()->SetTitleOffset(1.6);
             h->Sumw2();
-            h->Scale(1./EntryCount, "width");
+            // h->Scale(1./EntryCount, "width");
             h->SetMarkerStyle(20);
             h->SetMarkerSize(0.9);
             h->SetMarkerColor(colorIdx);
             h->SetLineColor(colorIdx);
             h->SetStats(0);
-            h->Draw();
-            c->Update();
-            c->SaveAs("test.jpg");
-            PdfFile.AddCanvas(c);
+            if (i == 0) {
+                hNStack->Add(h);
+                legend->AddEntry(h, legendLabel, "p"); // we only need to add this once
+            } else {
+                hPTStack->Add(h);
+            }
+            // hPTStack->Add(h);
+            // h->Draw();
+            // c->Update();
+            // c->SaveAs("test.jpg");
+            // PdfFile.AddCanvas(c);
         }
 
         TCanvas* cscatter = new TCanvas("cscatter", "", 1200, 900);
-        Int_t n = nsFiltered.size();
 
-        Double_t x[n], y[n];
-        for (Int_t i=0; i<n; i++) {
-            x[i] = nsFiltered.at(i);
-            y[i] = ptsFiltered.at(i);
-        }
-        
-        TGraph *gr = new TGraph(n, x, y);
-        // gr->SetLineColor(2);
-        // gr->SetLineWidth(4);
-        gr->SetMarkerColor(colorIdx);
-        gr->SetMarkerSize(0.8);
-        // gr->SetMarkerStyle(21);
-        gr->SetTitle(("pT vs Constituent Multiplicity" + titleMod).c_str());
-        gr->GetXaxis()->SetTitle("Constituent Multiplicity");
-        gr->GetYaxis()->SetTitle("pT");
-        gr->Draw("ap");
+        h2->Draw("COLZ");
         cscatter->Update();
-        cscatter->SaveAs("test_scatter.jpg");
+        cscatter->SaveAs("testscatter.jpg");
         PdfFile.AddCanvas(cscatter);
 
-        // HJetN->Sumw2();
-        // HJetN->Scale(1./EntryCount, "width");
-        // HJetN->SetMarkerStyle(20);
-        // HJetN->SetMarkerSize(0.9);
-        // HJetN->SetStats(0);
+        std::vector<double> v; 
+        std::vector<double> v_error;
+        for (int i = 0; i < HJetN->GetNbinsX(); i++) {
+            v.push_back(HJetN->GetBinContent(i + 1)); 
+            v_error.push_back(HJetN->GetBinError(i + 1));
+        }
 
-        // hNStack->Add(HJetN);
-        // hPTStack->Add(HJetPT);
-
-
-        // std::vector<double> v; 
-        // for (int i = 0; i < HJetN.GetNbinsX(); i++) {
-        //     v.push_back(HJetN.GetBinContent(i + 1)); 
-        // }
-
-        // // copy(v.begin(), v.end(), output_iterator);
-        // outFile << label << ",";
-        // for (const auto &e : v) outFile << e << ",";
+        // put csv lines in csv
+        // copy(v.begin(), v.end(), output_iterator);
+        outFile << label << ",";
+        for (const auto &e : v) outFile << e << ",";
+        outFile << endl;
+        outFile << label << ",";
+        for (const auto &e : v_error) outFile << e << ",";
+        outFile << endl;
 
         // PdfFile.AddPlot(HJetN, "", true);
         // PdfFile.AddPlot(HJetPT, "", true);
@@ -186,16 +189,25 @@ int main(int argc, char *argv[])
         
    }
 
-    // gStyle->SetHistTopMargin(0.25);
-    // hNStack->Draw("nostackb");
-    // cn->Update();
-    // cn->SaveAs("stack_test.jpg");
-    // hPTStack->Draw("nostackb");
+    TCanvas* cn = new TCanvas("cn","",1200,900);
+    hNStack->Draw("nostackb");
+    legend->SetTextSize(0.028);
+    legend->SetFillColor(0);
+    legend->SetBorderSize(1);
+    legend->Draw("");
+    cn->Update();
+    cn->SaveAs("test.jpg");
+    PdfFile.AddCanvas(cn);
 
-    // cout << firstBin << endl;
-    // hs->GetXaxis()->SetRange(firstBin-2, lastBin+2);
-    // cout << maxY << endl;
-    // hs->Draw("nostackb");
+    TCanvas* cpt = new TCanvas("cpt","",1200,900);
+    hPTStack->Draw("nostackb");
+    legend->SetTextSize(0.028);
+    legend->SetFillColor(0);
+    legend->SetBorderSize(1);
+    legend->Draw("");
+    cpt->Update();
+    cpt->SaveAs("testpt.jpg");
+    PdfFile.AddCanvas(cpt);
 
    PdfFile.AddTimeStampPage();
    PdfFile.Close();
